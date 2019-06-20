7f4e53ff55ff40bf5af4a8673652e35f1de97ee4
David Nordeen <davidnordeen@gmail.com>
2019-06-20 16:52:30 -0700

    updated server.js

--- src/server.js
+++ src/server.js
@@ -7,11 +7,16 @@ const { PORT } = require('./config');
 var db = knex({
   client: 'pg',
   connection: {
+    // port: '5432',
+    // host: '127.0.0.1',
+    // user: 'david',
+    // password: 'password',
+    // database: 'NBA'
     port: '5432',
-    host: '127.0.0.1',
-    user: 'david',
-    password: 'password',
-    database: 'NBA'
+    host: 'ec2-23-21-186-85.compute-1.amazonaws.com',
+    user: 'btzggybkvnwudt',
+    password: '72e9a61707b49234869959707ad9b1dc07b3b09ae243aea63bff2b383bbb9b26',
+    database: 'dtvn39d8uelen'
   },
   debug: true
 });
