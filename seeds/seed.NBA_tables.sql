ac9774a185fa0dfbd4c889a156a2d386bd7f093c
David Nordeen <davidnordeen@gmail.com>
2019-08-19 13:00:48 -0700

    update seeds

--- seeds/seed.NBA_tables.sql
+++ seeds/seed.NBA_tables.sql
@@ -11,15 +11,16 @@ TRUNCATE
 INSERT INTO NBA_players (name, team, position, age, rank, id)
 VALUES
   ('Giannis Antetokounmpo', 'Milwaukee Bucks', 'SF', 24, 1, 1),
-  ('Lebron James', 'LA Lakers', 'SF', 24, 2, 2),
-  ('Anthony Davis',  'LA Lakers', 'SF', 24, 3, 3),
-  ('James Harden',  'Houston Rockets', 'SF', 24, 4, 4),
-  ('Stephen Curry', 'Golden State Warriors', 'SF', 24, 5, 5),
-  ('Kawhi Leonard',  'Toronto Raptors', 'SF', 24, 6, 6),
+  ('Lebron James', 'LA Lakers', 'SF', 34, 2, 2),
+  ('Anthony Davis',  'LA Lakers', 'SF', 27, 3, 3),
+  ('James Harden',  'Houston Rockets', 'SF', 29, 4, 4),
+  ('Stephen Curry', 'Golden State Warriors', 'SF', 30, 5, 5),
+  ('Kawhi Leonard',  'Toronto Raptors', 'SF', 29, 6, 6),
   ('Karl-Anthony Towns',  'Minnesota Timberwolves', 'SF', 24, 7, 7),
   ('Nikola Jokic',  'Denver Nuggets', 'SF', 24, 8, 8),
-  ('Russell Westbrook',  'Oklahoma City Thunder', 'SF', 24, 9, 9),
-  ( 'Paul George',  'Oklahoma City Thunder', 'SF', 24, 10, 10);
+  ('Russell Westbrook',  'Oklahoma City Thunder', 'SF', 30, 9, 9),
+  ( 'Paul George',  'Oklahoma City Thunder', 'SF', 29, 10, 10),
+  ( 'Bradley Beal',  'Washington Wizards', 'SG', 26, 10, 10);
 
 COMMIT;
 
