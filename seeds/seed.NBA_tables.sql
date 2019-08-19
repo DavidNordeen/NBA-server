d80fa37f9ea0c358dce1a45eca8cdb23a9be9256
David Nordeen <davidnordeen@gmail.com>
2019-08-19 13:07:16 -0700

    update seeds

--- seeds/seed.NBA_tables.sql
+++ seeds/seed.NBA_tables.sql
@@ -20,7 +20,17 @@ VALUES
   ('Nikola Jokic',  'Denver Nuggets', 'SF', 24, 8, 8),
   ('Russell Westbrook',  'Oklahoma City Thunder', 'SF', 30, 9, 9),
   ( 'Paul George',  'Oklahoma City Thunder', 'SF', 29, 10, 10),
-  ( 'Bradley Beal',  'Washington Wizards', 'SG', 26, 10, 10);
+  ( 'Bradley Beal',  'Washington Wizards', 'SG', 26, 11, 11),
+  ( 'Joel Embiid',  'Philadelphia 76ers', 'C', 26, 12, 12),
+  ( 'Ben Simmons',  'Philadelphia 76ers', 'PG', 22, 13, 13),
+  ( 'Nikola Vucevic',  'Orlando Magic', 'PG', 28, 14, 14),
+  ( 'Andre Drummond',  'Detroit Pistons', 'PG', 26, 15, 15),
+  ( 'Kevin Durant',  'Brooklyn Nets', 'SF', 31, 16, 16),
+  ( 'Rudy Gobert',  'Utah Jazz', 'C', 28, 17, 17),
+  ( 'Clint Capela',  'Houston Rockets', 'C', 25, 18, 18),
+  ( 'Damian Lillard',  'Portland Trailblazers', 'PG', 30, 19, 19),
+  ( 'Jrue Holiday',  'New Orleans Pelicans', 'PG', 30, 20, 20),
+  ;
 
 COMMIT;
 
